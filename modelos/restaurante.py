cd4fbe9da64d1b8346ab5bf869b7f5d77cddf539
VMurtis <v.murtis@gmail.com>
2026-07-24 19:42:35 -0300

    adiciona instancia de restaurante e atribuicao de atributos

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -4,8 +4,13 @@ class Restaurante:
     ativo = False
 
 restaurante_praca = Restaurante()
+restaurante_praca.nome = 'Praça'
+restaurante_praca.categoria = 'Gourmet'
+
 restaurante_pizza = Restaurante()
 
 restaurantes = [restaurante_praca, restaurante_pizza]
 
-print(restaurantes)
+
+print(dir(restaurante_praca))# retorna uma lista com todos os atributos e metodos disponiveis no objeto
+print(restaurante_praca.ativo)
