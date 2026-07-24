b25a45e4f34d4228f969f380486bb4bd7b713e6c
VMurtis <v.murtis@gmail.com>
2026-07-24 20:06:07 -0300

    refatora classe restaurante com metodo __init__ e uso de vars

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,16 +1,18 @@
 class Restaurante:
-    nome = ''
-    categoria = ''
-    ativo = False
+     def __init__(self, nome, categoria):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
 
-restaurante_praca = Restaurante()
-restaurante_praca.nome = 'Praça'
-restaurante_praca.categoria = 'Gourmet'
 
-restaurante_pizza = Restaurante()
+restaurante_praca = Restaurante('Praça', 'Gourmet')
+restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
+
 
 restaurantes = [restaurante_praca, restaurante_pizza]
 
 
-print(dir(restaurante_praca))# retorna uma lista com todos os atributos e metodos disponiveis no objeto
-print(restaurante_praca.ativo)
+
+print(vars(restaurante_praca))
+print(vars(restaurante_pizza))
+
