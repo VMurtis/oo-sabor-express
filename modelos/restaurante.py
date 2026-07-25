48503f5c6869b65f3136e60103dbf79116e96424
VMurtis <v.murtis@gmail.com>
2026-07-24 21:29:16 -0300

    refatora atributos para encapsulamento

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -2,18 +2,18 @@ class Restaurante:
     restaurantes = []
 
     def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
         self._ativo = False
         Restaurante.restaurantes.append(self)
     
     def __str__(self):
-        return f'{self.nome} | {self.categoria}'
+        return f'{self._nome} | {self.categoria}'
     
     def listar_restaurantes():
         print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'status'}')
         for restaurante in Restaurante.restaurantes:
-            print(f"{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}")
+            print(f"{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante._ativo}")
 
     @property
     def ativo(self):
@@ -23,6 +23,7 @@ class Restaurante:
         self._ativo = not self._ativo
 
 restaurante_praca = Restaurante('Praça', 'Gourmet')
+restaurante_praca._nome = "Praça 2.0"
 restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
 
 Restaurante.listar_restaurantes()
